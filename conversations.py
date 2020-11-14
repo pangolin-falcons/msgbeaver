83af0c34670722081ccf0d398ce48c9f037feba3
Vagish Vela <web@vagish.com>
2020-11-14 11:32:51 -0500

    Add conversations class stub

---
+++ conversations.py
@@ -0,0 +1,10 @@
+class Conversations:
+
+    def __init__(self, token):
+        self.token = token
+
+    def send(self, message):
+        return
+
+    def receive(self, message):
+        return
