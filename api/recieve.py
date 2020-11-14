66522d511f11d5b68286371c05aa9efe3b15ac01
Vagish Vela <web@vagish.com>
2020-11-14 14:33:36 -0500

    Add API endpoint for new sms

--- api/recieve.py
+++ api/recieve.py
@@ -1,6 +1,22 @@
-from flask import Flask
+from flask import Flask, request
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
 
 @app.route('/')
+
 def hello_world():
     return 'Hello, World!'
+
+sms = {}
+
+class ReceiveSMS(Resource):
+    def put(self, newsms_id):
+        sms[newsms_id] = request.form['data']
+        return {newsms_id: sms[newsms_id]}
+
+api.add_resource(ReceiveSMS, '/<string:newsms_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
