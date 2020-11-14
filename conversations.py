3e10e5ba3d3bd5051c0c3abaaddc75d0e5de2f5a
Vagish Vela <web@vagish.com>
2020-11-14 13:32:49 -0500

    Refactor send sms into class

--- conversations.py
+++ conversations.py
@@ -1,10 +1,52 @@
+import boto3
+from botocore.exceptions import ClientError
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class Conversations:
 
-    def __init__(self, token):
-        self.token = token
+    def __init__(self):
+        region = "us-east-1"
+        self.client = boto3.client('pinpoint', region_name=region)
 
-    def send(self, message):
-        return
+    def send(self, phoneNum, message):
+
+        originationNumber = "+19706237718"
+        destinationNumber = phoneNum
+
+        applicationId = os.getenv("APP_ID")
+        messageType = "PROMOTIONAL"
+
+        registeredKeyword = "HELP"
+
+        senderId = "msgbeaver"
+
+        try:
+            response = self.client.send_messages(
+                ApplicationId=applicationId,
+                MessageRequest={
+                    'Addresses': {
+                        destinationNumber: {
+                            'ChannelType': 'SMS'
+                        }
+                    },
+                    'MessageConfiguration': {
+                        'SMSMessage': {
+                            'Body': message,
+                            'Keyword': registeredKeyword,
+                            'MessageType': messageType,
+                            'OriginationNumber': originationNumber,
+                            'SenderId': senderId
+                        }
+                    }
+                }
+            )
+
+        except ClientError as e:
+            return ["Failure", e.response['Error']['Message']]
+        else:
+            return ["Success", response['MessageResponse']['Result'][destinationNumber]['MessageId']]
 
     def receive(self, message):
         return
--- main.py
+++ main.py
@@ -1,45 +1,5 @@
 from conversations import Conversations
-import boto3
-from botocore.exceptions import ClientError
 
-region = "us-east-1"
-
-originationNumber = "+19706237718"
-destinationNumber = "+19378421922"
-
-message = ("We cannot deny or confirm.")
-applicationId = "f1dcce6b6bcd4e789ad5609e847a3941"
-messageType = "PROMOTIONAL"
-
-registeredKeyword = "HELP"
-
-senderId = "msgbeaver"
-
-client = boto3.client('pinpoint',region_name=region)
-
-try:
-    response = client.send_messages(
-        ApplicationId=applicationId,
-        MessageRequest={
-            'Addresses': {
-                destinationNumber: {
-                    'ChannelType': 'SMS'
-                }
-            },
-            'MessageConfiguration': {
-                'SMSMessage': {
-                    'Body': message,
-                    'Keyword': registeredKeyword,
-                    'MessageType': messageType,
-                    'OriginationNumber': originationNumber,
-                    'SenderId': senderId
-                }
-            }
-        }
-    )
-
-except ClientError as e:
-    print(e.response['Error']['Message'])
-else:
-    print("Message sent! Message ID: "
-            + response['MessageResponse']['Result'][destinationNumber]['MessageId'])
+conv = Conversations()
+status = conv.send("+17404173920","Hello Ian!")
+print(status)
