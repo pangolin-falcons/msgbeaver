ecfda20c42a5a6fc6c0f936358792b02c9e46510
Spencer Wolf <spwolf18@gmail.com>
2020-11-15 01:25:48 -0500

    I am so sorry

--- store.py
+++ store.py
@@ -1,65 +1,33 @@
+import sqlite3 as sql
 from dispatcher import Dispatcher
+
 class Store:
 
     def __init__(self):
-        self.requests = [
-        {'p_user': '9378421922', 
-         'p_vendor': None,
-         'time': '2000-01-01T00:00:00',
-         'message': 'give me the meats', 
-         'accepted': False},
-        {'p_user': '9378421922', 
-         'p_vendor': '8004849999',
-         'time': '2000-01-01T00:00:00',
-         'message': 'I have big grass help me', 
-         'accepted': 'True'},
-        {'p_user': '9378421922', 
-         'p_vendor': '8004849999',
-         'time': '2000-01-01T00:00:00',
-         'message': 'Yeet a hampster for me', 
-         'accepted': False},
-        {'p_user': '9378421922', 
-         'p_vendor': None,
-         'time': '2000-01-01T00:00:00',
-         'message': 'I need grocieriez run pop tarts are out', 
-         'accepted': False}
-        ]
-        self.users = [
-        {'phone': '+19378421922',
-         'name': 'spencer',
-         'type': 'vendor',
-         'address': '4203 Falcon Rd\n Pangolin, OH',
-         'keyword': 'carpenter'
-         },
-        {'phone': '+17176494887',
-         'name': 'fake vendor',
-         'type': 'vendor',
-         'address': 'Ham Street',
-         'keyword': 'locksmith'
-         },
-        ]
+        self.db_name = "data.sqlite"
 
     ## Reviever Utilities ##
 
-    def storeRequest(self, phoneNum, messageBody, timestamp):
-        new_request = dict([
-            ('p_user', phoneNum),
-            ('p_vendor', -1),
-            ('time', timestamp),
-            ('message', messageBody),
-            ('accepted', False)])
-        # Puts data into store
-        self.requests.append(new_request)
-        print("Request %s stored" % messageBody)
-        dispatch = Dispatcher()
-        print("passed to dispatch")
-        dispatch.processRequest(phoneNum, messageBody, self.users)
-
-
+    def storeRequest(self, phoneNum, messageBody):
+        conn = sql.connect(self.db_name)
+        conn.execute('''
+            INSERT INTO Orders (c_id, request)
+                VALUES (
+                    (SELECT c.c_id FROM Customers c WHERE c.phoneNumber = ?), 
+                    ?
+                );''', (phoneNum, messageBody))
+        conn.commit()
+        conn.close()
 
     def acceptRequest(self, vendor_phone):
         # Get vendor orders where accepted = false
             # To accept mark the order as accepted2
+        conn = sql.connect(self.db_name)
+        conn.execute('''
+            UPDATE request r
+                SET r.is_accepted = 1
+                WHERE r.is_accepted = 0 AND 
+        ''', ())
         for r in self.requests:
             if(r['accepted'] == False and r['p_vendor'] == vendor_phone):
                 r['accepted'] = True
@@ -71,6 +39,8 @@ class Store:
             if(r['accepted'] == False and r['p_vendor'] == vendor_phone):
                 r['p_vendor'] = None
         print("Reqest to vendor %s accepted" % vendor_number)
+
+    def markCompletedRequest(self, vendor_phone): 
 
     ## Dispatcher Utilities ##
 
@@ -88,3 +58,4 @@ class Store:
         # Removes request data at index
         print("Purging registered request data")
         return
+
